3d2b3c130c396f32740a5e337db8655944a748d0
zzallang <wmfsp9@naver.com>
2023-08-04 16:50:06 +0900

    vue.js ref 사용 전

---
+++ src/App_reactive.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<p>{{ message.value }}</p>
+		<button v-on:click="addMessage">add click</button>
+	</div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+	setup() {
+		let message = reactive({
+			value: 'Hello Vue!',
+		});
+		const addMessage = () => {
+			message.value = message.value + '!';
+		};
+		console.log('message ', message);
+		console.log('message typeof ', typeof message);
+		return {
+			message,
+			addMessage,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
