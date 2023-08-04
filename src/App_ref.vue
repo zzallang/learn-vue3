1c18b6e875a9f5c21b45adf301188cd25da52b74
zzallang <wmfsp9@naver.com>
2023-08-04 17:29:14 +0900

    반응형 기초_reactivity_ref 사용 후

--- src/App_ref.vue
+++ src/App_ref.vue
@@ -18,6 +18,7 @@ export default {
 		console.log('message');
 		console.log('message ', message.value);
 		console.log('message typeof ', typeof message.value);
+
 		return {
 			message,
 			addMessage,
