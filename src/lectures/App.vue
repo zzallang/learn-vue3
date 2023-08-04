bdbbfaa6a1a267caf77871b8da642653c37e597e
zzallang <wmfsp9@naver.com>
2023-08-04 17:41:13 +0900

    .

---
+++ src/lectures/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		const count = ref(0);
+		const state = reactive({
+			count,
+		});
+		console.log(count.value);
+		console.log('state.count: ', state.count);
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
