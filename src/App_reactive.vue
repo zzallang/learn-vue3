9bdf10641aaaade1b4cb0183a0a22e2d1d9081cb
zzallang <wmfsp9@naver.com>
2023-08-04 17:31:51 +0900

    App_reactive 수정

--- src/App_reactive.vue
+++ src/App_reactive.vue
@@ -1,10 +1,6 @@
 <template>
 	<div>
-<<<<<<< HEAD
 		<p>{{ message }}</p>
-=======
-		<p>{{ message.value }}</p>
->>>>>>> main
 		<button v-on:click="addMessage">add click</button>
 	</div>
 </template>
@@ -20,11 +16,7 @@ export default {
 		const addMessage = () => {
 			message.value = message.value + '!';
 		};
-<<<<<<< HEAD
 		console.log('message ', message.value);
-=======
-		console.log('message ', message);
->>>>>>> main
 		console.log('message typeof ', typeof message);
 		return {
 			message,
