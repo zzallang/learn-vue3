75b1cbc4ad9d144900fdaacd407c6121fc50fc43
zzallang <wmfsp9@naver.com>
2023-08-04 18:01:56 +0900

    ref_object_array

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,8 @@
 </template>
 
 <script>
-import { reactive, ref } from 'vue';
-
 export default {
 	setup() {
-		const count = ref(0);
-		const state = reactive({
-			count,
-		});
-		console.log(count.value);
-		console.log('state.count: ', state.count);
 		return {};
 	},
 };
