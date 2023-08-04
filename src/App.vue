4fc572923f4f2850dcff29a4e4975153d613315a
zzallang <wmfsp9@naver.com>
2023-08-04 17:40:35 +0900

    반응형 기초_반응형 객체의 ref Unwarpping

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,16 @@
 </template>
 
 <script>
+import { reactive, ref } from 'vue';
+
 export default {
 	setup() {
+		const count = ref(0);
+		const state = reactive({
+			count,
+		});
+		console.log(count.value);
+		console.log('state.count: ', state.count);
 		return {};
 	},
 };
