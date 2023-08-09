f6476cd4fa6d495abd460c2d42fee4038ee2d7d5
zzallang <wmfsp9@naver.com>
2023-08-09 16:59:15 +0900

    v-memo

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <template>
-	<div>
-		<p>{{ msg }}</p>
-		<p v-text="msg"></p>
-		<p v-html="htmlStr"></p>
-		<p v-text="htmlStr"></p>
-		<p v-pre>{{ msg 안녕하세요 }}</p>
-		<p v-once>{{ msg }} !!!!!</p>
-	</div>
+	<div></div>
 </template>
 
 <script>
-import { ref } from 'vue';
-
 export default {
 	setup() {
-		const msg = ref('Hi');
-		const htmlStr = ref('<Strong>Hi!!!!</Strong>');
-
-		return { msg, htmlStr };
+		return {};
 	},
 };
 </script>
