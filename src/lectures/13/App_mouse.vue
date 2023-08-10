786c7c6d6a414462d7f85f5c8a66a1f803b762d1
zzallang <wmfsp9@naver.com>
2023-08-10 10:29:19 +0900

    mouse-event

---
+++ src/lectures/13/App_mouse.vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+		<div id="modifiers">
+			<div @click="clickDiv">
+				DIV 영역
+				<p @click.self="clickP">
+					P영역
+					<!-- <span @click.stop="clickSpan"> SPAN 영역 </span> -->
+					<span @click="clickSpan"> SPAN 영역 </span>
+					<a href="http://naver.com" @click.prevent.stop="clickA">a 영역</a>
+				</p>
+			</div>
+		</div>
+		<button @click.once="clickDiv">once</button>
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {
+		const clickDiv = () => {
+			console.log('clickDiv');
+			// location.href = 'http://naver.com';
+		};
+		const clickP = () => {
+			console.log('clickP');
+		};
+		const clickSpan = () => {
+			console.log('clickSpan');
+			// alert('good');
+		};
+		const clickA = () => {
+			alert('어떤 기능?');
+		};
+		return { clickDiv, clickP, clickSpan, clickA };
+	},
+};
+</script>
+
+<style scoped>
+#modifiers div,
+#modifiers p,
+#modifiers span {
+	padding: 40px;
+}
+#modifiers div {
+	background-color: #ccc;
+}
+#modifiers p {
+	background-color: #999;
+}
+#modifiers span {
+	background-color: #666;
+	display: block;
+}
+</style>
