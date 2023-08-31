50694958909d4f248ddc8370cfaca0bfa56d420a
zzallang <wmfsp9@naver.com>
2023-08-31 14:31:32 +0900

    watchEffect

---
+++ src/lectures/15/App_watchEffect.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<form @submit.prevent>
+			<input v-model.lazy="title" type="text" placeholder="title" />
+			<textarea v-model="contents" placeholder="contents"></textarea>
+			<hr />
+			<button @click="save(title, contents)">저장</button>
+		</form>
+	</div>
+</template>
+
+<script>
+import { ref, watch, watchEffect } from 'vue';
+
+export default {
+	setup() {
+		const title = ref('');
+		const contents = ref('');
+
+		const save = (title, contents) => {
+			console.log('저장 완료 title : ' + title + ' contents : ' + contents);
+		};
+
+		watchEffect(() => {
+			console.log('watchEffect');
+			save(title.value, contents.value);
+		});
+
+		return { title, contents, save };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
