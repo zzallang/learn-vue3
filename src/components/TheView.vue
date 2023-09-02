0547689f47026bc568b6b77fdea5b96010dffbf6
zzallang <wmfsp9@naver.com>
2023-09-02 23:30:30 +0900

    bootstrap

---
+++ src/components/TheView.vue
@@ -0,0 +1,26 @@
+<template>
+	<main>
+		<div class="container py-4">
+			<div class="container text-center">
+				<div class="row g-2">
+					<div class="col col-4"></div>
+					<div class="col col-4"></div>
+					<div class="col col-4"></div>
+					<div class="col col-4"></div>
+					<div class="col col-4"></div>
+					<div class="col col-4"></div>
+				</div>
+			</div>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
