b26b82f3a684d81a5d5418324ed28899562a911f
zzallang <wmfsp9@naver.com>
2023-09-06 00:13:30 +0900

    props_문자열 배열 선언

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,10 +1,9 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title red">Card title</h5>
+			<h5 class="card-title red">{{ title }}</h5>
 			<p class="card-text">
-				Some quick example text to build on the card title and make up the bulk
-				of the card's content.
+				{{ contents }}
 			</p>
 			<a href="#" class="btn btn-primary">Go somewhere</a>
 		</div>
@@ -16,32 +15,11 @@ import { ref, useCssModule } from 'vue';
 
 console.log('AppCard module');
 export default {
+	props: ['title', 'contents'],
 	setup() {
-		// const style = useCssModule();
-		// console.log('sytle: ', style);
-		console.log('AppCard setup()');
-
-		const color = ref('red');
-		color.value = 'gray';
-		return { color };
+		return {};
 	},
 };
 </script>
 
-<style>
-.red {
-	color: v-bind(color) !important;
-}
-</style>
-
-<!-- <style module="classes">
-.red {
-	color: red !important;
-}
-</style> -->
-
-<!-- <style scoped>
-.red {
-	color: red !important;
-}
-</style> -->
+<style></style>
--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -2,25 +2,9 @@
 	<main>
 		<div class="container py-4">
 			<div class="container text-center">
-				<div class="row g-2">
-					<div class="col col-4">
-						<!-- <AppCard></AppCard> -->
-						<app-card></app-card>
-					</div>
-					<div class="col col-4">
-						<AppCard></AppCard>
-					</div>
-					<div class="col col-4">
-						<AppCard></AppCard>
-					</div>
-					<div class="col col-4">
-						<AppCard></AppCard>
-					</div>
-					<div class="col col-4">
-						<AppCard></AppCard>
-					</div>
-					<div class="col col-4">
-						<AppCard></AppCard>
+				<div class="row g-3">
+					<div v-for="post in posts" :key="post.id" class="col col-4">
+						<AppCard :title="post.title" :contents="post.contents"></AppCard>
 					</div>
 				</div>
 			</div>
@@ -30,12 +14,25 @@
 
 <script>
 import AppCard from '@/components/AppCard.vue';
+import { reactive } from 'vue';
 export default {
 	components: {
 		AppCard,
 	},
 	setup() {
-		return {};
+		const post = reactive({
+			title: '제목2',
+			contents: '내용2',
+		});
+
+		const posts = reactive([
+			{ id: 1, title: '제목1', contents: '내용1' },
+			{ id: 2, title: '제목2', contents: '내용2' },
+			{ id: 3, title: '제목3', contents: '내용3' },
+			{ id: 4, title: '제목4', contents: '내용4' },
+			{ id: 5, title: '제목5', contents: '내용5' },
+		]);
+		return { post, posts };
 	},
 };
 </script>
