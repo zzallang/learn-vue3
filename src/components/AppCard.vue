148f67cfeceb2be80b7e6a37cc8970ed25e2d7af
zzallang <wmfsp9@naver.com>
2023-09-05 23:37:23 +0900

    SFC

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title">Card title</h5>
+			<h5 class="card-title red">Card title</h5>
 			<p class="card-text">
 				Some quick example text to build on the card title and make up the bulk
 				of the card's content.
@@ -12,13 +12,36 @@
 </template>
 
 <script>
+import { ref, useCssModule } from 'vue';
+
 console.log('AppCard module');
 export default {
 	setup() {
+		// const style = useCssModule();
+		// console.log('sytle: ', style);
 		console.log('AppCard setup()');
-		return {};
+
+		const color = ref('red');
+		color.value = 'gray';
+		return { color };
 	},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.red {
+	color: v-bind(color) !important;
+}
+</style>
+
+<!-- <style module="classes">
+.red {
+	color: red !important;
+}
+</style> -->
+
+<!-- <style scoped>
+.red {
+	color: red !important;
+}
+</style> -->
